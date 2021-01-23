26739a98f24f63347870d0962715af581a7ab4cb
Rodrigo <rodrigoventura97@live.com>
2021-01-23 11:06:03 -0300

    Creating component to bring a label with the pokemon type

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,19 +5,22 @@ import { FormInputComponent } from './components/form-input/form-input.component
 import { NavbarComponent } from './navbar/navbar.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TypesComponent } from './types/types.component';
 
 @NgModule({
     declarations: [
         // App
         FooterComponent,
         FormInputComponent,
-        NavbarComponent
+        NavbarComponent,
+        TypesComponent
     ],
     exports: [
         // App
         FooterComponent,
         FormInputComponent,
-        NavbarComponent
+        NavbarComponent,
+        TypesComponent
     ],
     imports: [
         // Angular
---
+++ src/app/shared/types/types.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-types',
+  templateUrl: './types.component.html',
+  styleUrls: ['./types.component.scss']
+})
+export class TypesComponent implements OnInit {
+
+  @Input() type: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
